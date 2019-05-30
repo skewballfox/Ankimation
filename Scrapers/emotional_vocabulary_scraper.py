befbba309040d6ec0f8b20da5e5adcda61804a71
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2019-05-29 20:21:40 -0500

    started the scrapers

---
+++ Scrapers/emotional_vocabulary_scraper.py
@@ -0,0 +1,4 @@
+import requests
+from bs4 import BeautifulSoup
+
+url= 'https://www.verywellfamily.com/feelings-words-from-a-to-z-2086647'
---
+++ Scrapers/git_glossary_scaper.py
@@ -0,0 +1,4 @@
+import requests
+from bs4 import BeautifulSoup
+
+url= 'https://git-scm.com/docs/gitglossary'
