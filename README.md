e0409e556ca0cd3ad3cbab95e16802c2a8489638
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2021-03-09 13:07:08 -0600

    well, this is long overdue

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Ankimation
+
+a description of the directories
+
+## Scrapers
+the quick an dirty scrapers that I have written, in the hopes of making something general, other than the scraper functions which may be useful for you, the rest should be ignored. 
+
+## Builders
+the files I use to turn information grabbed by the scrapers into decks
+
+
--- Scrapers/scraper_functions.py
+++ Scrapers/scraper_functions.py
@@ -1,42 +1,58 @@
 import requests
+from typing import Any
+from bs4 import BeautifulSoup as bs
 
-from bs4 import BeautifulSoup
 
-def save_html(html, path):
-    """save html to file
-    uses 'rb' (write bytes) in order to avoid encoding issues"""
-    with open(path, 'wb') as f:
-        f.write(html)
+def get_soup(url) -> Any:
+    request = requests.get(url)
+    return bs(request.content, "html.parser")
 
-def open_html(path):
+
+def open_soup(path):
     """
     open locally saved html as file
-    uses 'rb' (read bytes) in order to avoid encoding issues"""
-    with open(path, 'rb') as f:
-        return f.read()
+    """
+    with open(path, "r") as f:
+        return bs(f.read(), "html.parser")
 
-def fd_lookup(word, type='adj'):
-    base_url  = "https://www.thefreedictionary.com/"
+
+def save_html(html, filename):
+    """save html to file
+    uses 'rb' (write bytes) in order to avoid encoding issues"""
+    with open(filename, "w") as f:
+        f.write(html)
+
+
+def fd_lookup(word, type="adj"):
+    base_url = "https://www.thefreedictionary.com/"
     query_url = base_url + word
     request = requests.get(query_url)
-    soup = BeautifulSoup(request.content, 'html.parser')
-    print('finding sections')
-    #NOTE: check for existence of h2 in section, as only definitions
-    #have it. 
-    for item in soup.find_all('section', 'data-src'):
-            print (item.text)
-    for item in soup.find_all('div', {'class':'ds-list'}):
-        #check for existence in order to avoid error
-        #note could probably be moved to inside try catch block
-        if item.find('b') is not None:
-                if '1.' in item.find('b').text:
-                        try: 
-                                print (item.contents[next(i for i, content in enumerate(item.contents) if '1.' in content.text)+1])
-                        except AttributeError:
-                                print("the tested content isn't text")
-                                
+    soup = BeautifulSoup(request.content, "html.parser")
+    print("finding sections")
+    # NOTE: check for existence of h2 in section, as only definitions
+    # have it.
+    for item in soup.find_all("section", "data-src"):
+        print(item.text)
+    for item in soup.find_all("div", {"class": "ds-list"}):
+        # check for existence in order to avoid error
+        # note could probably be moved to inside try catch block
+        if item.find("b") is not None:
+            if "1." in item.find("b").text:
+                try:
+                    print(
+                        item.contents[
+                            next(
+                                i
+                                for i, content in enumerate(item.contents)
+                                if "1." in content.text
+                            )
+                            + 1
+                        ]
+                    )
+                except AttributeError:
+                    print("the tested content isn't text")
 
 
-#Definition > section:nth-child(2) > div:nth-child(3) > div:nth-child(5)
-if __name__=="__main__":
+# Definition > section:nth-child(2) > div:nth-child(3) > div:nth-child(5)
+if __name__ == "__main__":
     print(fd_lookup("angry"))
